03ed8a1b21fdaee88d59069adbe9314a2f39d16a
Bhupendra-Singh284 <bhupendrasinghshekhawat284@gmail.com>
2024-07-15 10:02:05 +0530

    inital project commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "News_app_jetpack_compose_mvvm"
+include(":app")
